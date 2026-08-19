2147dd2f271dcebf505a7c70ae94f6663ea9a338
Sobir Mamasoliyev <mamasoliyevs538@gmail.com>
2026-08-19 23:57:50 +0500

    fixed.....

--- src/pages/server/server.js
+++ src/pages/server/server.js
@@ -4,8 +4,8 @@ import axios from "axios"
 // BASE_URL = "10.113.144.60";
 
 const api = axios.create({
-    baseURL: `http://127.0.0.1:8000`,
-    // baseURL: `https://idiom-princess-catnap.ngrok-free.dev`,
+    // baseURL: `http://127.0.0.1:8000`,
+    baseURL: `https://idiom-princess-catnap.ngrok-free.dev`,
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
